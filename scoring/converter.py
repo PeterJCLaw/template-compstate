e128cab0b47bc4939dd6f39dba8f625ef9f17b76
Peter Law <PeterJCLaw@gmail.com>
2024-10-19 17:09:14 +0100

    Clarify that this is needed

--- scoring/converter.py
+++ scoring/converter.py
@@ -1,7 +1,11 @@
 """
 Converter to assist the SRComp Scorer UI reading & writing score files.
 
-Optional part of a compstate.
+This should be updated in tandem with `update.html` which is used to render the
+scores and define the form inputs.
+
+Required as part of a compstate, though the default implementation may suffice
+for simple cases.
 """
 
 from __future__ import annotations
